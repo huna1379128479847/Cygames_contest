a2bd2088b544e82f3f13a320e89acfa0cb33a466
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2024-09-11 23:37:18 +0900

    続き

--- Assets/Scripts/Bases/Enums.cs
+++ Assets/Scripts/Bases/Enums.cs
@@ -6,18 +6,29 @@ using System.Threading.Tasks;
 
 namespace Contest
 {
+    //敵Aiの思考パターンを示す
     public enum BehaviorPattern
     {
-        Aggressive,
-        Defensive,
-        Support
+        Aggressive,//攻撃的
+        Defensive,//防御的
+        Support//支援型
     }
 
+    //そのユニットの種別を示す
     public enum UnitType
     {
-        Friend,
-        Enemy,
-        FriendAI,
-        EnemyAI,
+        Friend,//味方
+        Enemy,//敵
+        FriendAI,//味方AI
+        EnemyAI,//敵AI
     }
+
+    //BattleSceneで使用:現在のターンを示す
+    public enum Tarn
+    {
+        Friend,//味方のターン
+        Enemy,//敵のターン
+    }
+
+
 }
--- Assets/Scripts/Bases/Interfaces.cs
+++ Assets/Scripts/Bases/Interfaces.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 namespace Contest
 {
-    public interface IUnit
+    public interface IUnit//ユニット用
     {
         UnitType UnitType { get; }
         string Name { get; }
         void Update();
     }
 
-    public interface IEnemy
+    public interface IEnemy//敵ユニット用
     {
         BehaviorPattern BehaviorPattern { get; }
         void EnemyBehavior();
     }
 
-    public interface IStats
+    public interface IStats//ユニット用
     {
         int MaxHP { get; }
-        int CurrentHP { get; }
+        int CurrentHP { get; }//現在HP
         int MaxMP { get; }
         int CurrentMP { get; }
         int MaxSpeed { get; }
